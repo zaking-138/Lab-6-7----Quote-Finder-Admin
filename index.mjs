b8dd93419509f2b152fc27847f7abe87fc7f5a92
Zach <zjpk23@gmail.com>
2025-12-01 12:00:22 -0800

    4

--- index.mjs
+++ index.mjs
@@ -34,18 +34,7 @@ const [rowsCategories] = await pool.query('SELECT DISTINCT category FROM `quotes
 app.get('/', (req, res) => {
     res.render('login.ejs')
 });
-// app.get('/test', async (req, res) =>{
-//     let accessToken = "48027fa6e5ae4e90a670bb831279bbe6"
 
-//     let response = await fetch('https://api.spotify.com/v1/search?q=remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis&type=album', {
-//         headers: {
-//             Authorization: 'Bearer' + accessToken
-//         }
-//     })
-//     let data = await response.json()
-//     console.log(data);
-//     res.redirect('/')
-// })
 app.post('/attemptLogin', async (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
